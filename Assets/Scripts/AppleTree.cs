bfc17dc82b5ddd552000e478a24f0a6b73374306
Tarakur Rahman <39719842+tarakurrahman@users.noreply.github.com>
2018-06-05 23:49:55 -0400

    Made edit in the AppleTree Scripts
    - Made changes to the AppleTree script and linked it to AppleTree prefab

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class AppleTree : MonoBehaviour {
-
-	// Use this for initialization
+	[Header("Set in Inspector")]
+	// Prefab for instantiating apples
+	public GameObject applePrefab;
+	// Speed at which the AppleTree moves
+	public float speed = 1f;
+	// Distance where AppleTree turns around
+	public float leftAndRightEdge = 10f;
+	// Chance that the AppleTree will change directions
+	public float chanceToChangeDirections = 0.1f;
+	// Rate at which Apples will be instantiated
+	public float secondsBetweenAppleDrops = 1f;
 	void Start () {
-		
+		// Dropping apples every second
 	}
-	
-	// Update is called once per frame
 	void Update () {
-		
+		// Basic Movement
+		// Changing Direction
 	}
 }
