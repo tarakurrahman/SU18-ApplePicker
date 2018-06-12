78f17a4edff0b578ed428ebf988484c832f4df63
Tarakur Rahman <39719842+tarakurrahman@users.noreply.github.com>
2018-06-12 02:02:49 -0400

    Instantiating the baskets
    - The baskets are now at the bootom of the screen
    - The basket does not act
    - The next step is to make the basket move with the mouse

--- Assets/Scripts/ApplePicker.cs
+++ Assets/Scripts/ApplePicker.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ApplePicker : MonoBehaviour {
+	[Header("Set in Inspector")] // a
+	public GameObject basketPrefab;
+	public int numBaskets = 3;
+	public float basketBottomY = -14f;
+	public float basketSpacingY = 2f;
 
-	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+		for (int i=0; i<numBaskets; i++) {
+			GameObject tBasketGO = Instantiate<GameObject>( basketPrefab );
+			Vector3 pos = Vector3.zero;
+			pos.y = basketBottomY + ( basketSpacingY * i );
+			tBasketGO.transform.position = pos;
+		}
 	}
 }
