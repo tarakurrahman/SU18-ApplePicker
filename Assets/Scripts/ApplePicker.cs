0e8a4f1c56a5cc5c9bd4e394f95c992a2c57ebca
Tarakur Rahman <39719842+tarakurrahman@users.noreply.github.com>
2018-06-12 12:31:35 -0400

    We are now able to destroy a basket when an apple is dropped
    - Next step is to add the high scores

--- Assets/Scripts/ApplePicker.cs
+++ Assets/Scripts/ApplePicker.cs
@@ -1,28 +1,41 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
+using System.Collections.Generic; // a
 using UnityEngine;
+using UnityEngine.SceneManagement; // b
 
 public class ApplePicker : MonoBehaviour {
-	[Header("Set in Inspector")] // a
+	[Header("Set in Inspector")]
 	public GameObject basketPrefab;
 	public int numBaskets = 3;
 	public float basketBottomY = -14f;
 	public float basketSpacingY = 2f;
+	public List<GameObject> basketList;
 
 	void Start () {
+		basketList = new List<GameObject>(); // c
 		for (int i=0; i<numBaskets; i++) {
 			GameObject tBasketGO = Instantiate<GameObject>( basketPrefab );
 			Vector3 pos = Vector3.zero;
 			pos.y = basketBottomY + ( basketSpacingY * i );
 			tBasketGO.transform.position = pos;
+			basketList.Add( tBasketGO ); // d
 		}
 	}
 
-	public void AppleDestroyed() { // a
+	public void AppleDestroyed() {
 		// Destroy all of the falling apples
-		GameObject[] tAppleArray=GameObject.FindGameObjectsWithTag("Apple"); // b
+		GameObject[] tAppleArray=GameObject.FindGameObjectsWithTag("Apple");
 		foreach ( GameObject tGO in tAppleArray ) {
 			Destroy( tGO );
 		}
+	
+		// Destroy one of the baskets // e
+		// Get the index of the last Basket in basketList
+		int basketIndex = basketList.Count-1;
+		// Get a reference to that Basket GameObject
+		GameObject tBasketGO = basketList[basketIndex];
+		// Remove the Basket from the list and destroy the GameObject
+		basketList.RemoveAt( basketIndex );
+		Destroy( tBasketGO );
 	}
 }
