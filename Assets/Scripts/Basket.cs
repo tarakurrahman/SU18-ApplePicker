ab4ea9196c3616e4677d22b163dfabeeb2b84e40
Tarakur Rahman <39719842+tarakurrahman@users.noreply.github.com>
2018-06-12 12:17:08 -0400

    We can now add points for each caught apple
    - The next step is to notify apple picker that an apple was dropped

--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -1,8 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; // This line enables use of uGUI features. // a
 
 public class Basket : MonoBehaviour {
+	[Header("Set Dynamically")]
+	public Text scoreGT; // a
+
+	void Start() {
+		// Find a reference to the ScoreCounter GameObject
+		GameObject scoreGO = GameObject.Find("ScoreCounter"); // b
+		// Get the Text Component of that GameObject
+		scoreGT = scoreGO.GetComponent<Text>(); // c
+		// Set the starting number of points to 0
+		scoreGT.text = "0";
+	}
 
 	void Update () {
 		// Get the current screen position of the mouse from Input
@@ -20,11 +32,17 @@ public class Basket : MonoBehaviour {
 		this.transform.position = pos;
 	}
 
-	void OnCollisionEnter( Collision coll ) { // a
+	void OnCollisionEnter( Collision coll ) {
 		// Find out what hit this basket
-		GameObject collidedWith = coll.gameObject; // b
-		if ( collidedWith.tag == "Apple" ) { // c
+		GameObject collidedWith = coll.gameObject;
+		if ( collidedWith.tag == "Apple" ) {
 			Destroy( collidedWith );
+			// Parse the text of the scoreGT into an int
+			int score = int.Parse( scoreGT.text ); // d
+			// Add points for catching the apple
+			score += 100;
+			// Convert the score back to a string and display it
+			scoreGT.text = score.ToString();
 		}
 	}
 }
