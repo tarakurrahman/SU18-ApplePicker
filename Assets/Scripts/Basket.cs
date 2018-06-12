1a9817dab26462c2c1e1b7326e2f35fabcb0805d
Tarakur Rahman <39719842+tarakurrahman@users.noreply.github.com>
2018-06-12 02:09:55 -0400

    Added the ability to move the baskets with the mouse
    - Not able to catch apples
    - Next step is to catch the apples

--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class Basket : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
 	void Update () {
-		
+		// Get the current screen position of the mouse from Input
+		Vector3 mousePos2D = Input.mousePosition; // a
+
+		// The Camera's z position sets how far to push the mouse into 3D
+		mousePos2D.z = -Camera.main.transform.position.z; // b
+
+		// Convert the point from 2D screen space into 3D game world space
+		Vector3 mousePos3D = Camera.main.ScreenToWorldPoint( mousePos2D ); // c
+
+		// Move the x position of this Basket to the x position of the Mouse
+		Vector3 pos = this.transform.position;
+		pos.x = mousePos3D.x;
+		this.transform.position = pos;
 	}
 }
